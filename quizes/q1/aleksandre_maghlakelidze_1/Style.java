e02401779983aa71ca278adc6bd6686fc1f894a1
paatagog <paatagog@gmail.com>
2024-03-11 00:10:53 +0400

    Initial commit

---
+++ quizes/q1/aleksandre_maghlakelidze_1/Style.java
@@ -0,0 +1,12 @@
+package quizes.q1.aleksandre_maghlakelidze_1;
+
+public class Style {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
